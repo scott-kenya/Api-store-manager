32645b7883fc29317e58ae46028c45ce87906e09
Scott Francis <scotfrank28@gmail.com>
2018-10-18 23:11:09 +0300

    [#161268738] Adding a product

--- app/api/v1/products.py
+++ app/api/v1/products.py
@@ -1,1 +1,41 @@
-products.py
+from flask import Flask, make_response, jsonify, request
+from flask_restful import Resource, Api, reqparse
+
+#from app.api.models import products
+
+products = []
+
+
+
+
+
+
+
+
+
+
+	def post(name):
+			"""Endpoint for adding new pdt"""
+			data = request.get_json()
+			if not data:
+				return jsonify({"message": "field cannot be empty"})
+			name = data['name']
+			price = data['price']
+			product_id = len(products)+1
+			quantity = data['quantity']
+			if not name or name == "":
+				return jsonify({"message": "Please enter product name"}), 404
+			else:
+
+				payload = {
+				'name': name,
+				'price': price,
+				'product_id': product_id,
+				'quantity': quantity
+				}
+
+				products.append(payload)
+				
+				return make_response(jsonify({'list': products}), 201)
+
+		
