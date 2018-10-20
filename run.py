ff154e950f83b70e9b773dee0d3dca6a9958ca35
Scott Francis <scotfrank28@gmail.com>
2018-10-20 16:22:05 +0300

    trial

--- run.py
+++ run.py
@@ -3,11 +3,8 @@ from app import create_app
 app = create_app("development")
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return '<h1> hi Scott!!! <h1>'
 
 
 if __name__ == '__main__':
-	port = int(os.environ.get('PORT', 5000))
-   	app.run(host='0.0.0.0', port=port)
+	#port = int(os.environ.get('PORT', 5000))
+   	app.run(debug=True)
