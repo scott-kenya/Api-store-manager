4395cf443dfda91ee3ba3aa12f6ab8ee68fe7113
Scott Francis <scotfrank28@gmail.com>
2018-10-25 23:33:59 +0300

    [#161492923]creating registration and login

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -9,9 +9,12 @@ from app.api.v1.views.users_view import LoginUser
 zed = Blueprint("api", __name__, url_prefix="/api/v1")
 api = Api(zed)
 
+
 api.add_resource(Products, '/products')
 api.add_resource(Product_id, '/products/<int:product_id>')
 api.add_resource(Sales, '/sales')
 api.add_resource(Sale_id, '/sales/<int:sale_id>')
 api.add_resource(Register, '/auth/register')
 api.add_resource(LoginUser, '/auth/login')
+api.add_resource(users, '/createUser')
+
